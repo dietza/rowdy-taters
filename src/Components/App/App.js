ab6e89112c84da90f9597aa3e94b0bb1fa1165c8
Kristen Bair <kristenmbair@gmail.com>
2021-02-03 18:06:17 -0700

    Add initial class component structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,3 +1,23 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Movies from '../Movies/Movies';
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Movies />
+      </>
+    )
+  }
+}
 
 export default App;
