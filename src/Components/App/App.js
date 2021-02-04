e0577f5b92b501fe45f03738d79c5931972c89e4
Kristen Bair <kristenmbair@gmail.com>
2021-02-03 18:17:58 -0700

    Creat movie dummy data and import into App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Movies from '../Movies/Movies';
+import movieData from '../../movieData'
 
 class App extends Component {
   constructor() {
