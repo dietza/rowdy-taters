337bd0ec0fdf8c71a2db8169e2050f3a1f4b5c50
Kristen Bair <kristenmbair@gmail.com>
2021-02-08 16:51:11 -0700

    Test return to home button is present with correct text

--- cypress/integration/details_spec.js
+++ cypress/integration/details_spec.js
@@ -8,4 +8,10 @@ describe('Rowdy Taters', () => {
     cy.get('h1').contains('Rowdy Taters!')
   });
 
+  it('Has a button called All Movies', () => {
+  cy.get('.return-to-home-view-btn').contains('All Movies')
+    .get('.details__back-arrow').should('have.attr', 'src', '/static/media/double-left-arrows.79365501.svg')
+  });
+
+  
 });
