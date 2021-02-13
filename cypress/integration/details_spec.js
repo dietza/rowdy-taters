42c71d43db8cbf0b9567e5f7249ea3e48256eac9
Allison Dietz <dietz.allison@outlook.com>
2021-02-12 20:40:47 -0700

    Add initial test for display of movie trailer on details view

--- cypress/integration/details_spec.js
+++ cypress/integration/details_spec.js
@@ -65,9 +65,9 @@ describe('Money Plane Details view', () => {
     cy.get('.details__movie-genres').contains('Genre: Action')
   });
 
-  // it('Displays movie trailers', () => {
-  //   cy.get('iframe').click({force: true})
-  // });  ??????? Cant figure out how to test the trailer!
+  it('Displays movie trailers', () => {
+    cy.get('div div iframe').should('be.visible')
+  });
 
   it('All Movies button can take you back to main page', () => {
     cy.get('.return-to-home-view-btn').click()
