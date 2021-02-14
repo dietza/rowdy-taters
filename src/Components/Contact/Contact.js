9be9097d789cd461391e94a65cb8459880f58f91
Kristen Bair <kristenmbair@gmail.com>
2021-02-14 13:34:29 -0700

    Add content to Contact component, import photos, add page styling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import backArrow from '../../double-left-arrows.svg';
+import AD from '../../AD.jpg'
+import KB from '../../KB1.jpg'
 import './Contact.css'
 
 const Contact = () => {
@@ -15,13 +17,23 @@ const Contact = () => {
         </Link>
         <h1>Contact Us</h1>
         <section className="contacts">
-          <article>
-            <img src="" alt="BEAUTIFUL ALLISON"/>
-            <p>link to Allison's amazing github</p>
+          <article className="contact-info">
+            <div className="prof-pic-wrap">
+            <img src={AD} alt="BEAUTIFUL ALLISON" className="prof-pic"/>
+            </div>
+            <a 
+              href="https://github.com/dietza" 
+              target="_blank" 
+              className="github-link">Allison D</a>
           </article>
-          <article>
-            <img src="" alt="BEAUTIFUL KB"/>
-            <p>link to KB's amazing github</p>
+          <article className="contact-info">
+            <div className="prof-pic-wrap">
+              <img src={KB} alt="BEAUTIFUL KB" className="prof-pic"/>
+            </div>
+            <a 
+              href="https://github.com/kristenmb" 
+              target="_blank" 
+              className="github-link">Kristen B</a>
           </article>
         </section>
       </section>
