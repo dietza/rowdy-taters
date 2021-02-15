42f48c636e6da565d26510ab34af38716b407b87
Allison Dietz <dietz.allison@outlook.com>
2021-02-15 14:16:52 -0700

    Clean up to remove comments from tests

--- cypress/integration/rowdy_taters_spec.js
+++ cypress/integration/rowdy_taters_spec.js
@@ -67,26 +67,22 @@ describe('Rowdy Taters main page view', () => {
   });
 
   it ('Each movie card should display a poster image',  () => {
-      // specifically targets the PENINSULA movie card
     cy
       .get('#581392')
       .find('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//sy6DvAu72kjoseZEjocnm2ZZ09i.jpg')
   });
 
   it ('Each movie card should display a title',  () => {
-      // specifically targets the PENINSULA movie card
     cy
       .get('#581392 .movie-title').contains('Peninsula')
   });
 
   it ('Each movie card should display a release year',  () => {
-      // specifically targets the PENINSULA movie card
     cy
       .get('#581392 .movie-release').contains('2020')
   });
 
   it ('Each movie card should display a tater rating',  () => {
-      // specifically targets the PENINSULA movie card
     cy
       .get('#581392 .movie-rating').contains('Tater Rating: 7.0')
   });
