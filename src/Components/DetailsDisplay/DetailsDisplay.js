5aab112cbdbee493d2ea8a7bd4d45851df89cf8b
Kristen Bair <kristenmbair@gmail.com>
2021-02-16 17:24:05 -0700

    Add conditional render on details page to account for any missing data from api

--- src/Components/DetailsDisplay/DetailsDisplay.js
+++ src/Components/DetailsDisplay/DetailsDisplay.js
@@ -5,6 +5,7 @@ import './DetailsDisplay.css';
 
 const DetailsDisplay = ({ movieToDisplay, trailersToDisplay }) => {
   const releaseYear = movieToDisplay.release_date.split('-')[0]
+  const runtime = movieToDisplay.runtime
   const rating = movieToDisplay.average_rating.toFixed(1)
   const genres = movieToDisplay.genres.map(genre => {
     return <p key={genre} className='details__each-genre'>{genre}</p>
@@ -24,24 +25,35 @@ const DetailsDisplay = ({ movieToDisplay, trailersToDisplay }) => {
         <p className='details__movie-rating'>
           {`Tater Rating: ${rating}`}
         </p>
-        <p className='details__movie-release'>
-          {`${releaseYear} | ${movieToDisplay.runtime} mins`}
-        </p>
-        <p className='details__movie-overview'>
-          {movieToDisplay.overview}
-        </p>
-          {!genres && 
-          <div> No Genres to display
-          </div>}
-          {genres && 
-          <div className='details__movie-genres'>
-          Genre: {genres}
-          </div>}
+
+        {releaseYear && runtime !== 0 && <p className='details__movie-release'>
+          {`${releaseYear} | ${runtime} mins`}
+        </p>}
+        {releaseYear && runtime === 0 && <p className='details__movie-release'>
+          {`${releaseYear}`}
+        </p>}
+        {!releaseYear && runtime !== 0 && <p className='details__movie-release'>
+          {`${runtime} mins`}
+        </p>}
+        
+        {movieToDisplay.overview && <p className='details__movie-overview'> {movieToDisplay.overview} </p>}
+        {!movieToDisplay.overview && <p>No synopsis provided</p>}
+
+        {!genres && 
+        <div> No Genres to display
+        </div>}
+        {genres && 
+        <div className='details__movie-genres'>
+        Genre: {genres}
+        </div>}
+
         {trailersToDisplay.length > 0 && 
         <ReactPlayer 
           key={ trailersToDisplay.id }
           width={ '50vw' }
-          url={`https://www.youtube.com/watch?v=${trailersToDisplay[0].key}`} />}
+          url={`https://www.youtube.com/watch?v=${trailersToDisplay[0].key}`} 
+        />}
+
     </article> 
     </>
   )
